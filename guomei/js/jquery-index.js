9ab69e5ee7d4266bea16c266a4f9ccf1f01aa94c
limbobark <limbobark@gmail.com>
2017-02-06 11:02:19 +0800

    gome updata

--- guomei/js/jquery-index.js
+++ guomei/js/jquery-index.js
@@ -9,13 +9,7 @@ $(".ywm-nav-user").hover(function(){
 		$(".ywm-nav-username-hover").attr("class","ywm-nav-username");
 		$(this).children(".ywm-user-dropdown").eq(0).css("display","none");
 })
-$(".ywm-nav-user").hover(function(){
-		$(".ywm-nav-username").attr("class","ywm-nav-username-hover");
-		$(this).children(".ywm-user-dropdown").eq(0).css("display","block");
-	},function(){
-		$(".ywm-nav-username-hover").attr("class","ywm-nav-username");
-		$(this).children(".ywm-user-dropdown").eq(0).css("display","none");
-})
+
 // 购物车
 $(".ywm-shopcartbox").hover(function(){
 	    $(".ywm-menulink").addClass("ywm-hover");
